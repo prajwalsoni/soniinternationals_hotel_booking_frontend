dd1223e5220aca417dfa2a2769f45a93687a7df1
prajwalsoni <prajwalsonics@gmail.com>
2024-02-12 15:24:43 +0530

    Added Layer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,40 +1,40 @@
-// import React from "react";
-// import { FaInstagram, FaFacebookF, FaGithub, FaLinkedinIn } from 'react-icons/fa';
-// import { Container, Row, Col } from 'react-bootstrap';
+import React from "react";
+import { FaInstagram, FaFacebookF, FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { Container, Row, Col } from 'react-bootstrap';
 
-// const Footer = () => {
-//     let today = new Date()
-//     return (
-//         <footer className="bg-dark text-light py-3 footer mt-lg-5">
-//             <Container>
-//                 <Row className="align-items-center">
-//                     <Col xs={12} md={6} className="text-md-left mb-3 mb-md-0">
-//                         <a href="https://www.instagram.com/prajwal_soni__/" target="_blank" rel="noopener noreferrer" className="btn btn-outline-light mr-2">
-//                             <FaInstagram /> Instagram
-//                         </a>
-//                         <a href="https://www.facebook.com/prajwal.soni.944?mibextid=rS40aB7S9Ucbxw6v" target="_blank" rel="noopener noreferrer" className="btn btn-outline-light mr-2">
-//                             <FaFacebookF /> Facebook
-//                         </a>
-//                         <a href="https://github.com/prajwalsoni" target="_blank" rel="noopener noreferrer" className="btn btn-outline-light mr-2">
-//                             <FaGithub /> GitHub
-//                         </a>
-//                         <a href="https://www.linkedin.com/in/prajwal-soni-a67671259/" target="_blank" rel="noopener noreferrer" className="btn btn-outline-light">
-//                             <FaLinkedinIn /> LinkedIn
-//                         </a>
-//                     </Col>
-//                     <Col xs={12} md={6} className="text-right">
-//                         <p className="mb-0">
+const Footer = () => {
+    let today = new Date()
+    return (
+        <footer className="bg-dark text-light py-3 footer mt-lg-5">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} className="text-md-left mb-3 mb-md-0">
+                        <a href="https://www.instagram.com/prajwal_soni__/" target="_blank" rel="noopener noreferrer" className="btn btn-outline-light mr-2">
+                            <FaInstagram /> Instagram
+                        </a>
+                        <a href="https://www.facebook.com/prajwal.soni.944?mibextid=rS40aB7S9Ucbxw6v" target="_blank" rel="noopener noreferrer" className="btn btn-outline-light mr-2">
+                            <FaFacebookF /> Facebook
+                        </a>
+                        <a href="https://github.com/prajwalsoni" target="_blank" rel="noopener noreferrer" className="btn btn-outline-light mr-2">
+                            <FaGithub /> GitHub
+                        </a>
+                        <a href="https://www.linkedin.com/in/prajwal-soni-a67671259/" target="_blank" rel="noopener noreferrer" className="btn btn-outline-light">
+                            <FaLinkedinIn /> LinkedIn
+                        </a>
+                    </Col>
+                    <Col xs={12} md={6} className="text-right">
+                        <p className="mb-0">
                            
-//                             <a > Terms & Conditions</a> | 
-//                             <a > Privacy Policy</a> | 
-//                             <a > Security</a> |
-// 							  Soni Internationals  {new Date().getFullYear()} &copy;
-//                         </p>
-//                     </Col>
-//                 </Row>
-//             </Container>
-//         </footer>
-//     );
-// }
+                            <a > Terms & Conditions</a> | 
+                            <a > Privacy Policy</a> | 
+                            <a > Security</a> |
+							  Soni Internationals  {new Date().getFullYear()} &copy;
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+}
 
-// export default Footer;
+export default Footer;
--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,94 +1,94 @@
-// import React, { useContext, useState } from "react"
-// import { NavLink, Link } from "react-router-dom"
-// import Logout from "../auth/Logout"
+import React, { useContext, useState } from "react"
+import { NavLink, Link } from "react-router-dom"
+import Logout from "../auth/Logout"
 
 
-// const NavBar = () => {
-// 	const [showAccount, setShowAccount] = useState(false)
+const NavBar = () => {
+	const [showAccount, setShowAccount] = useState(false)
 
-// 	const handleAccountClick = () => {
-// 		setShowAccount(!showAccount)
-// 	}
+	const handleAccountClick = () => {
+		setShowAccount(!showAccount)
+	}
 
-// 	const isLoggedIn = localStorage.getItem("token")
-// 	const userRole = localStorage.getItem("userRole")
+	const isLoggedIn = localStorage.getItem("token")
+	const userRole = localStorage.getItem("userRole")
 
-// 	return (
-// 		<nav className="navbar navbar-expand-lg bg-body-tertiary px-5 shadow mt-5 sticky-top">
-// 			<div className="container-fluid">
-// 				<Link to={""} className="navbar-brand">
-// 					<a href="https://www.linkedin.com/in/prajwal-soni-a67671259/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
-// 						<span className="navbar-brand hotel-color" style={{ fontSize: '1.3em', fontWeight: 'bold', fontFamily: 'cursive' }}>Soni Internationals</span>
-// 					</a>
+	return (
+		<nav className="navbar navbar-expand-lg bg-body-tertiary px-5 shadow mt-5 sticky-top">
+			<div className="container-fluid">
+				<Link to={""} className="navbar-brand">
+					<a href="https://www.linkedin.com/in/prajwal-soni-a67671259/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+						<span className="navbar-brand hotel-color" style={{ fontSize: '1.3em', fontWeight: 'bold', fontFamily: 'cursive' }}>Soni Internationals</span>
+					</a>
 
-// 				</Link>
+				</Link>
 
-// 				<button
-// 					className="navbar-toggler"
-// 					type="button"
-// 					data-bs-toggle="collapse"
-// 					data-bs-target="#navbarScroll"
-// 					aria-controls="navbarScroll"
-// 					aria-expanded="false"
-// 					aria-label="Toggle navigation">
-// 					<span className="navbar-toggler-icon"></span>
-// 				</button>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarScroll"
+					aria-controls="navbarScroll"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
 
-// 				<div className="collapse navbar-collapse" id="navbarScroll">
-// 					<ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
-// 						<li className="nav-item">
-// 							<NavLink className="nav-link" aria-current="page" to={"/browse-all-rooms"}>
-// 								Browse all rooms
-// 							</NavLink>
-// 						</li>
+				<div className="collapse navbar-collapse" id="navbarScroll">
+					<ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+						<li className="nav-item">
+							<NavLink className="nav-link" aria-current="page" to={"/browse-all-rooms"}>
+								Browse all rooms
+							</NavLink>
+						</li>
 
-// 						{isLoggedIn && userRole === "ROLE_ADMIN" && (
-// 							<li className="nav-item">
-// 								<NavLink className="nav-link" aria-current="page" to={"/admin"}>
-// 									Manage Rooms
-// 								</NavLink>
-// 							</li>
-// 						)}
-// 					</ul>
+						{isLoggedIn && userRole === "ROLE_ADMIN" && (
+							<li className="nav-item">
+								<NavLink className="nav-link" aria-current="page" to={"/admin"}>
+									Manage Rooms
+								</NavLink>
+							</li>
+						)}
+					</ul>
 
-// 					<ul className="d-flex navbar-nav">
-// 						<li className="nav-item">
-// 							<NavLink className="nav-link" to={"/find-booking"}>
-// 								Find my booking
-// 							</NavLink>
-// 						</li>
+					<ul className="d-flex navbar-nav">
+						<li className="nav-item">
+							<NavLink className="nav-link" to={"/find-booking"}>
+								Find my booking
+							</NavLink>
+						</li>
 
-// 						<li className="nav-item dropdown">
-// 							<a
-// 								className={`nav-link dropdown-toggle ${showAccount ? "show" : ""}`}
-// 								href="#"
-// 								role="button"
-// 								data-bs-toggle="dropdown"
-// 								aria-expanded="false"
-// 								onClick={handleAccountClick}>
-// 								{" "}
-// 								Account
-// 							</a>
+						<li className="nav-item dropdown">
+							<a
+								className={`nav-link dropdown-toggle ${showAccount ? "show" : ""}`}
+								href="#"
+								role="button"
+								data-bs-toggle="dropdown"
+								aria-expanded="false"
+								onClick={handleAccountClick}>
+								{" "}
+								Account
+							</a>
 
-// 							<ul
-// 								className={`dropdown-menu ${showAccount ? "show" : ""}`}
-// 								aria-labelledby="navbarDropdown">
-// 								{isLoggedIn ? (
-// 									<Logout />
-// 								) : (
-// 									<li>
-// 										<Link className="dropdown-item" to={"/login"}>
-// 											Login
-// 										</Link>
-// 									</li>
-// 								)}
-// 							</ul>
-// 						</li>
-// 					</ul>
-// 				</div>
-// 			</div>
-// 		</nav>
-// 	)
-// }
+							<ul
+								className={`dropdown-menu ${showAccount ? "show" : ""}`}
+								aria-labelledby="navbarDropdown">
+								{isLoggedIn ? (
+									<Logout />
+								) : (
+									<li>
+										<Link className="dropdown-item" to={"/login"}>
+											Login
+										</Link>
+									</li>
+								)}
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	)
+}
 
-// export default NavBar
+export default NavBar
